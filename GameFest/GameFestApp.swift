43cf7d62956f5533bccb3b16b3e8b28b0a5b3162
Paul Merceur <paul.merceur83@gmail.com>
2023-03-16 12:29:43 +0100

    Initial Commit

---
+++ GameFest/GameFestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameFestApp.swift
+//  GameFest
+//
+//  Created by Paul Merceur on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct GameFestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
